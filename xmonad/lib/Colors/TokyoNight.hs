256b05d94c5bcba81c37322fba839a55d2473ef6
Liam Ederzeel <liamederzeel@gmail.com>
2022-06-06 18:33:32 +0200

    feat: add xmonad config

---
+++ xmonad/lib/Colors/TokyoNight.hs
@@ -0,0 +1,29 @@
+module Colors.TokyoNight where
+
+import XMonad
+
+colorScheme = "tokyo-night"
+
+colorBack = "#24283b"
+colorFore = "#c0caf5"
+
+color01 = "#1D202F"
+color02 = "#f7768e"
+color03 = "#7aa2f7"
+color04 = "#e0af68"
+color05 = "#7aa2f7"
+color06 = "#bb9af7"
+color07 = "#7dcfff"
+color08 = "#a9b1d6"
+color09 = "#414868"
+color10 = "#f7768e"
+color11 = "#9ece6a"
+color12 = "#e0af68"
+color13 = "#7aa2f7"
+color14 = "#bb9af7"
+color15 = "#7dcfff"
+color16 = "#c0caf5"
+
+colorTrayer :: String
+colorTrayer = "--tint 0x24283b"
+
