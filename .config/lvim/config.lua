5217cdc9d627e41b09a13f66cfa77ce82ed22a4d
Liam Ederzeel <liamederzeel@gmail.com>
2022-07-28 22:53:10 +0200

    feat: remove ctrl-s bind

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -5,12 +5,12 @@ lvim.format_on_save = true
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
 -- add your own keymapping
-lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 lvim.colorscheme = 'tokyonight'
 -- unmap a default keymapping
 -- lvim.keys.normal_mode["<C-Up>"] = false
 -- edit a default keymapping
 -- lvim.keys.normal_mode["<C-q>"] = ":q<cr>"
+-- lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 
 lvim.builtin.alpha.dashboard.section.header.val = {
      "⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣠⣤⣤⣴⣦⣤⣤⣄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ ",
