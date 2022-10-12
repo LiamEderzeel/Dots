c71ce3cca073835da600db947de193764f772ff1
Liam Ederzeel <liamederzeel@gmail.com>
2022-10-12 18:19:35 +0200

    feat: update neovim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,30 +1,32 @@
-local utils = require('utils')
+vim.o.clipboard = "unnamedplus" -- Copy paste between vim and everything else
 
-require('variables')
-require('config')
-require('plugins')
-require('keymappings')
-require('settings')
+-- local utils = require('utils')
 
-require('lsp')
+-- require('variables')
+-- require('config')
+-- require('plugins')
+-- require('keymappings')
+-- require('settings')
 
-local autocmds = require('autocmds')
+-- require('lsp')
 
-autocmds.define_augroups(O.autocommands)
+-- local autocmds = require('autocmds')
 
-require("lsp").config()
+-- autocmds.define_augroups(O.autocommands)
 
-local null_status_ok, null_ls = pcall(require, "null-ls")
-if null_status_ok then
-  null_ls.config {}
-  require("lspconfig")["null-ls"].setup {}
-end
+-- require("lsp").config()
 
-local lsp_settings_status_ok, lsp_settings = pcall(require, "nlspsettings")
-if lsp_settings_status_ok then
-  lsp_settings.setup {
-    config_home = os.getenv "HOME" .. "/.config/lvim/lsp-settings",
-  }
-end
+-- local null_status_ok, null_ls = pcall(require, "null-ls")
+-- if null_status_ok then
+--   null_ls.config {}
+--   require("lspconfig")["null-ls"].setup {}
+-- end
 
-utils.toggle_autoformat()
+-- local lsp_settings_status_ok, lsp_settings = pcall(require, "nlspsettings")
+-- if lsp_settings_status_ok then
+--   lsp_settings.setup {
+--     config_home = os.getenv "HOME" .. "/.config/lvim/lsp-settings",
+--   }
+-- end
+
+-- utils.toggle_autoformat()
