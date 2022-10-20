20f91ee8d55848ddcfe03f62a9e302eec9f31566
Liam Ederzeel <liamederzeel@gmail.com>
2022-10-20 11:44:22 +0200

    feat: add neoscroll to lunarvim

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -194,6 +194,24 @@ linters.setup {
 -- Additional Plugins
 lvim.plugins = {
   {
+    'karb94/neoscroll.nvim',
+    config = function()
+      require('neoscroll').setup({
+        mappings = {
+          '<C-u>',
+          '<C-d>',
+          '<C-b>',
+          '<C-f>',
+          '<C-y>',
+          '<C-e>',
+          'zt',
+          'zz',
+          'zb'
+        },
+      })
+    end
+  },
+  {
     "folke/trouble.nvim",
     cmd = "TroubleToggle",
   },
