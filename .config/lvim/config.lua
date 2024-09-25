da5936f542b7701ab8d3be68ddff2bdd9def2888
Liam Ederzeel <liamederzeel@gmail.com>
2024-09-25 19:13:43 +0200

    refactor: remove comment lvim zenmode

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -262,11 +262,7 @@ lvim.plugins = {
   {
     "folke/zen-mode.nvim",
     config = function()
-      require("zen-mode").setup {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-      }
+      require("zen-mode").setup {}
     end
   }
 }
