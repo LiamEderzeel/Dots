34843350c2c36193dcaf2034c8c0068fa6b7ba2b
Liam Ederzeel <liamederzeel@gmail.com>
2024-11-25 13:20:57 +0100

    feat: update packges

--- .nixos/common/pkgs.nix
+++ .nixos/common/pkgs.nix
@@ -15,6 +15,7 @@ let
   stable = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
+    wlr-randr
     neovim
     tmux
     git
@@ -74,6 +75,10 @@ let
     xz
     exfat
     gnome.gnome-disk-utility
+    docker-compose
+    unzip
+    jq
+    weston
   ];
 in
 {
