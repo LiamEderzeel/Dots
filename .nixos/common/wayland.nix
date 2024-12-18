e15ac421ef8cc95c3d84dd8f82dba399740979be
Liam Ederzeel <liamederzeel@gmail.com>
2024-12-18 12:36:30 +0100

    fix: nixos 24.11 hyprland

--- .nixos/common/wayland.nix
+++ .nixos/common/wayland.nix
@@ -3,6 +3,7 @@
   programs.hyprland = {
     enable = true;
     # package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+     withUWSM = true;
      xwayland = {
     	enable = true;
      };
