bdb97158121ba72b03722efdfbc5f99702337650
Liam Ederzeel <liamederzeel@gmail.com>
2025-01-20 13:15:08 +0100

    feat(apps): add mongodb tools

--- .nixos/common/pkgs.nix
+++ .nixos/common/pkgs.nix
@@ -11,6 +11,8 @@ let
     lazydocker
     nodePackages."@antfu/ni"
     nodejs_20
+    mongodb-compass
+    mongosh
   ];
   stable = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
